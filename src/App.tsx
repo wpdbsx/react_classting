c16b0c13992a4cb2306858ce25ec5bf261043da5
wpdbsx <wpdbsx@naver.com>
2023-10-20 11:00:25 +0900

    프로젝트 초기설정 (#1)
    1. 리액트 ,타입스크립트, Styled-component, resetCSS ,lint,prettier 설치

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Reset } from 'styled-reset'
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Reset />
+      <AppLayout>
+
+      </AppLayout>
+    </>
+  );
+}
+}
+
+export default App;
