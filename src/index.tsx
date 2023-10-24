6fa92d98137f7ccd457dd530b91753d4ce8a245a
wpdbsx <wpdbsx@naver.com>
2023-10-25 08:49:05 +0900

    세션 스토리지 로직 수정 (#14)
    1. 스토리지 저장 리덕스 모듈 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from "redux-persist";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './reduxSaga/store/configureStore';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from "redux-persist";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
--- src/reduxSaga/reducers/index.ts
+++ src/reduxSaga/reducers/index.ts
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage/session' // session storage
+import storage from 'redux-persist/lib/storage/session'
 import quiz from './quiz'
 
 const persistConfig = {
   key: 'react',
-  storage, //storage : 세션, 로컬 스토리지 중에서 저장할 스토리지를 지정한다. session 지정
-  whitelist: ['resource'], //스토리지에 저장할 리덕스 모듈을 나열한다.
+  storage, // storage : 세션, 로컬 스토리지 중에서 저장할 스토리지를 지정한다. session 지정
+  whitelist: ['quiz'], // 스토리지에 저장할 리덕스 모듈을 나열한다.
 }
 const rootReducer = combineReducers({
   quiz,
